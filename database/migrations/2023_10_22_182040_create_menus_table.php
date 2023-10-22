9e2ae59c6d52053f8a748fe9929784076e56ded8
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-10-23 02:25:28 +0700

    update: masukin gambar

---
+++ database/migrations/2023_10_22_182040_create_menus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_menu');
+            $table->string('desc_menu');
+            $table->integer('price');
+            $table->string('type_menu');
+            $table->string('img_menu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
