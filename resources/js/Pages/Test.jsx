a906725d7eed2260a2f1c8d653e1121d4a3a3fb9
NAS2203 <139289057+H0tt4ru@users.noreply.github.com>
2023-10-23 13:53:22 +0700

    Backend Database Makanan Fixed

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function test(){
+	const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          '/api/menus'
+        );
+        setMenu(response.data);
+      }catch(error){
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to the Menu Page</h1>
+      <ul>
+        {menu.map(item => (
+          <li key={item.id}>{item.name_menu}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
