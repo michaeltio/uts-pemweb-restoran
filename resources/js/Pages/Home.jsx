569682e8cc8727e7a2ff93c039c592f7b48068c6
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-10-24 23:05:07 +0700

    merge github tampilan aos dengan main

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,14 +1,13 @@
-import Categories from '@/Components/Home/Categories';
-import Footer from '@/Components/Home/Footer';
-import Greetings from '@/Components/Home/Greetings';
-import Opening from '@/Components/Home/Opening';
-import View from '@/Components/Home/View';
-import Navbar from '@/Layouts/Navbar';
-import { Head } from '@inertiajs/react';
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from 'react';
-
+import Categories from "@/Components/Home/Categories";
+import Footer from "@/Components/Home/Footer";
+import Greetings from "@/Components/Home/Greetings";
+import Opening from "@/Components/Home/Opening";
+import View from "@/Components/Home/View";
+import Navbar from "@/Layouts/Navbar";
+import { Head } from "@inertiajs/react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Welcome({ auth }) {
     let isAdmin = 0;
@@ -19,10 +18,10 @@ export default function Welcome({ auth }) {
         isAdmin = 0;
     }
 
-      //aos
-      useEffect(() => {
+    //aos
+    useEffect(() => {
         AOS.init(); // Initialize AOS in the useEffect hook
-      }, []); 
+    }, []);
 
     return (
         <>
