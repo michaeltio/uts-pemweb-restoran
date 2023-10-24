65c19759c580c34f076d17af91eb5d8f08694fe8
NAS2203 <139289057+H0tt4ru@users.noreply.github.com>
2023-10-24 23:12:52 +0700

    Captcha

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Checkbox from "@/Components/Checkbox";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
@@ -12,7 +12,11 @@ export default function Login({ status, canResetPassword }) {
         email: "",
         password: "",
         remember: false,
+        captcha: "",
     });
+
+    const [captcha, setCaptcha] = useState("");
+    const [captchaIncorrect, setCaptchaIncorrect] = useState(false);
 
     useEffect(() => {
         return () => {
@@ -20,11 +24,42 @@ export default function Login({ status, canResetPassword }) {
         };
     }, []);
 
+    useEffect(() => {
+        const code = generateRandomCode(5);
+        setCaptcha(code);
+    }, []);
+
     const submit = (e) => {
         e.preventDefault();
-
-        post(route("login"));
+        const captchaInput = data.captcha;
+        if (captchaInput === captcha) {
+            post(route("login"));
+        } else {
+            console.log("Captcha is incorrect. Please try again.");
+            setCaptchaIncorrect(true);
+            setTimeout(() => {
+                setCaptchaIncorrect(false);
+                resetForm();
+            }, 2000);
+        }
     };
+
+    function generateRandomCode(length) {
+        const characters =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let code = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            code += characters.charAt(randomIndex);
+        }
+        return code;
+    }
+
+    function resetForm() {
+        setData("email", "");
+        setData("password", "");
+        setData("captcha", "");
+    }
 
     return (
         <GuestLayout>
@@ -82,6 +117,28 @@ export default function Login({ status, canResetPassword }) {
                                 className="mt-2"
                             />
                         </div>
+                        <div className="mt-4">
+                            <InputLabel
+                                htmlFor="captcha"
+                                value={`Please enter the following CAPTCHA: ${captcha}`}
+                            />
+                            <TextInput
+                                id="captcha"
+                                type="text"
+                                name="captcha"
+                                className="mt-1 block w-full"
+                                autoComplete="off"
+                                required
+                                onChange={(e) =>
+                                    setData("captcha", e.target.value)
+                                }
+                            />
+                            {captchaIncorrect && (
+                                <p className="text-red-500">
+                                    Incorrect CAPTCHA. Please try again.
+                                </p>
+                            )}
+                        </div>
                         <div className="flex justify-between block mt-4">
                             <label className="flex items-center">
                                 <Checkbox
@@ -126,69 +183,6 @@ export default function Login({ status, canResetPassword }) {
                     </form>
                 </div>
             </div>
-            {/*
-            Default Format
-            <form onSubmit={submit}>
-                
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form> */}
         </GuestLayout>
     );
 }
